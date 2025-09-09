756042f29721b895833f1346b364b346b718681d
mkohanim <michel@nucore.ai>
2025-09-09 09:21:43 -0700

    added archiving

--- process_batch_completion.py
+++ process_batch_completion.py
@@ -31,7 +31,18 @@ ENDPOINT = "/v1/chat/completions" # you can also use /v1/embeddings, /v1/respons
 COMPLETION_WINDOW = "24h"         # 24h or 4h depending on availability in your account
 BATCH_MAX_LINES_PER_REQUEST = 1800  # max lines per batch request for OpenAI 
 
-def list_batches(client:OpenAI):
+# A dictionary of completion status/archived
+archives={}
+
+def is_archived(batch)->bool:
+    try:
+        b = archives[batch.id]
+        return b['status'] == 'archived'
+    except Exception as ex:
+        print (f"failed verifying {batch.id} is archived {str(ex)}")
+        return False
+
+def list_batches(client:OpenAI, include_archives:bool):
     """
     List all batches and their statuses.
     """
@@ -42,11 +53,38 @@ def list_batches(client:OpenAI):
         if not data:
             break
         for batch in data:
+            if not include_archives:
+                if is_archived(batch):
+                    continue
             yield batch
         # The SDK returns items sorted newest→oldest; use last id as cursor
         after = data[-1].id
         if not page.has_more:
             break
+
+def cancel_batches(client:OpenAI):
+    print(f"Cancelling all batches for ...")
+    for batch in list_batches(client, False):
+        try:
+            client.batches.cancel(batch.id)
+            print(f"Cancelled batch {batch.id}")
+        except Exception as e:
+            print(f"Error cancelling batch {batch.id}: {e}")
+            continue
+
+def archive_batches(client:OpenAI, path:Path):
+    print(f"Archiving all batches ...")
+    for batch in list_batches(client, True):
+        try:
+            archives[batch.id]={
+                "status":"archived"
+            }
+        except Exception as e:
+            print(f"Error archiving batch {batch.id}: {e}")
+            continue
+    with open(path, 'w') as fp:
+        json.dump(archives, fp)
+
 
 def download_result(client:OpenAI, batch, path):
     """Download a Files API asset to disk."""
@@ -104,8 +142,7 @@ def download_results(client:OpenAI, path:Path):
     outputs_all: List[str] = []
     errors_all: List[str]  = []
     try:
-        batches = list_batches(client)
-        for batch in batches:
+        for batch in list_batches(client, False):
             if batch.status == "cancelled":
                 print(f"{batch.id} is cancelled; ignoring ...")
                 continue
@@ -131,7 +168,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process batch completions (cancel, wait, list).")
     parser.add_argument("--output_path", type=str, help="Path to the output directory where the samples are stored. If none given, it will be printed to stdout.")
     parser.add_argument("--types", type=str, help="Type of training: properties, commands, routines, general.")
-    parser.add_argument("--operation", type=str, help="Operation to perform on the batches: cancel, list, process")
+    parser.add_argument("--operation", type=str, help="Operation to perform on the batches: cancel, list, process, archive")
 
     args = parser.parse_args()
 
@@ -143,6 +180,15 @@ if __name__ == "__main__":
     REFERENCE_DIR = Path(get_data_directory("customer_data", None))
     BATCHED_REQUESTS_DIR = Path(get_data_directory("datasets", "batched-requests"))
     OUTPUT_DIR = Path(get_data_directory("datasets", "batched-samples"))
+    ARCHIVED_FILE = BATCHED_REQUESTS_DIR/"archives.json"
+
+    if not ARCHIVED_FILE.exists():
+        with open(ARCHIVED_FILE, 'w') as fp:
+            json.dump(archives, fp)
+    else:
+        with open(ARCHIVED_FILE, 'r') as fp:
+            archives = json.load(fp)
+
 
     output_path = Path(args.output_path) if args.output_path else OUTPUT_DIR
 
@@ -152,15 +198,11 @@ if __name__ == "__main__":
     client = OpenAI(api_key=globals()[f"OPENAI_API_KEY_BATCH"])  # or use environment variable
     try:
         if operation == "cancel":
-            print(f"Cancelling all batches for {type}...")
-            for batch in list_batches(client):
-                try:
-                    client.batches.cancel(batch.id)
-                    print(f"Cancelled batch {batch.id}")
-                except Exception as e:
-                    print(f"Error cancelling batch {batch.id}: {e}")
+            cancel_batches(client)
         elif operation == "process":
             download_results(client, OUTPUT_DIR)
+        elif operation == "archive":
+            archive_batches(client, ARCHIVED_FILE)
 
         elif operation == "list":
             print(f"Listing all batches ...")
